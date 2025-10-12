d2a08ec8bc08257b74de2cc343089205b2885952
ofentse-sithole <otsithole07@gmail.com>
2025-10-13 00:28:38 +0200

    added the correct the app.js

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import PersonalLogin from "./PersonalUse/Authentication/PersonalLogin";
+import PersonalRegister from "./PersonalUse/Authentication/PersonalRegister";
+import PersonalForgotPassword from "./PersonalUse/Authentication/PersonalForgotPassword";
+import BusinessLogin from "./BusinessUse/Authentication/BusinessLogin";
+import BusinessRegister from "./BusinessUse/Authentication/BusinessRegister";
+import BusinessForgotPassword from "./BusinessUse/Authentication/BusinessForgotPassword";
 
+const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PersonalLogin">
+        <Stack.Screen name="PersonalLogin" component={PersonalLogin} />
+        <Stack.Screen name="PersonalRegister" component={PersonalRegister} />
+        <Stack.Screen name="PersonalForgotPassword" component={PersonalForgotPassword} />
+        <Stack.Screen name="BusinessLogin" component={BusinessLogin} />
+        <Stack.Screen name="BusinessRegister" component={BusinessRegister} />
+        <Stack.Screen name="BusinessForgotPassword" component={BusinessForgotPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
