6af6228a7b4a7f68731d64f4241ac801b44e9aea
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2025-10-26 22:04:16 +0000

    chore(config): switch SafeAreaView to react-native-safe-area-context, add expo-contacts and consolidate app.json plugins/permissions

--- PersonalUse/Authentication/PersonalRegister.js
+++ PersonalUse/Authentication/PersonalRegister.js
@@ -5,10 +5,10 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  SafeAreaView,
   Image,
   Alert,
 } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from "react-native-vector-icons/Ionicons";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
