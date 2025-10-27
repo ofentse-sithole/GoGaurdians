3a18251fba23ff4a9f915664e08f666448c7805b
ofentse-sithole <otsithole07@gmail.com>
2025-10-27 13:04:24 +0200

    feat(EmergencyContacts): add emergency contact list and implement contact retrieval

---
+++ constants/EmergencyContacts.js
@@ -0,0 +1,7 @@
+// South Africa emergency services quick dial list
+// Update numbers to your region if needed.
+export const SA_EMERGENCY_CONTACTS = [
+  { name: 'Police (SAPS)', number: '10111' },
+  { name: 'Ambulance/Fire (EMS)', number: '10177' },
+  { name: 'National Emergency', number: '112' },
+];
